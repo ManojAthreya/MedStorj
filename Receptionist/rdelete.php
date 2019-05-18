5be481aa89c48e0a3ad12e37293b4fe9b9d7015b
ManojAthreya <manoj2375@gmail.com>
2019-05-18 18:57:27 +0530

    Receptionist Module

---
+++ Receptionist/rdelete.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect("localhost","root","root","userdb");
+if(mysqli_connect_errno()){
+ echo 'Failed to connect to MySQL', mysqli_connect_errno();
+}
+
+$id=$_GET['id'];
+$result = mysqli_query($conn,"DELETE FROM patientdetails WHERE id=$id")
+or die(mysqli_error($conn));
+header('location:rtable.php');
+
+
+?>
